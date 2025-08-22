b80ce4dffad96d7299f736a0fa9106110f6d7fae
Dao Van Tinh <tinhdao9@gmail.com>
2025-08-22 17:19:30 +0700

    Day 1: setup frontend and backend with DB connection

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-blue-500 p-4">Tailwind chạy OK!</div>{" "}
+    </>
+  );
+}
+
+export default App;
